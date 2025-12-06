fa637a31687ef82dbdfd7293b5dab9d2fe9d813b
manishapalsam <manishasomayya@gmail.com>
2025-12-06 08:38:46 +0530

    api-gateway config file

--- api-gateway/src/main/java/com/dailycodebuffer/Main.java
+++ api-gateway/src/main/java/com/dailycodebuffer/Main.java
@@ -1,5 +1,10 @@
 package com.dailycodebuffer;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
